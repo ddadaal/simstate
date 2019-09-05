cd218d5fe507d381313b617f51681a966eee3243
Chen Junda <smallda@outlook.com>
2019-09-05 22:22:37 +0800

    add tests and examples

--- tests/useStore.spec.tsx
+++ tests/useStore.spec.tsx
@@ -1,7 +1,7 @@
 import { testStore, noParamStore } from "./common";
 import { mount } from "enzyme";
 import { useStore, StoreProvider, createStore } from "../src";
-import React, { useState, useCallback } from "react";
+import React from "react";
 
 describe("UseStore", () => {
 
@@ -34,11 +34,31 @@ describe("UseStore", () => {
 
   it("should update when store updated", () => {
     const store = createStore(testStore, 42);
+
+    const Component = () => {
+      const { value, setValue } = useStore(testStore);
+      return (
+        <div>
+          <span>{value}</span>
+          <button onClick={() => setValue(value + 1)}>Increment</button>
+        </div>
+      );
+    }
+
     const wrapper = mount(
       <StoreProvider stores={[store]}>
-
+        <Component />
       </StoreProvider>
     )
+
+    const span = wrapper.find("span");
+    const btn = wrapper.find("button");
+
+    expect(span.text()).toBe("42");
+    btn.simulate("click");
+    expect(span.text()).toBe("43");
+
+
   });
 
   it("should report error when using a store that is not provided", () => {
@@ -58,18 +78,59 @@ describe("UseStore", () => {
     expect(() => mount(<Component />)).toThrowError();
   });
 
-  it("should not update since UpdateBlocker blocks update", () => {
+  it("should update even UpdateBlocker blocks update", () => {
+    const store = createStore(testStore, 42);
 
-    const MyComponent: React.FC<{ value: number }> = ({ value }) => <span>{value}</span>;
-
-    const Component: React.FC = () => {
-      const [value, setValue] = useState(10);
-      const increment = useCallback(() => setValue(value + 1), [value]);
+    const Component = () => {
+      const { value, setValue } = useStore(testStore);
       return (
-        <UpdateBlocker>
-          <MyComponent value={value} />
-        </UpdateBlocker>
-      )
+        <div>
+          <span>{value}</span>
+          <button onClick={() => setValue(value + 1)}>Increment</button>
+        </div>
+      );
     }
+
+    const Root = () => (
+      <StoreProvider stores={[store]}>
+        <UpdateBlocker>
+          <Component />
+        </UpdateBlocker>
+      </StoreProvider>
+    );
+
+    const wrapper = mount(<Root />);
+
+    const span = wrapper.find("span");
+    const btn = wrapper.find("button");
+
+    expect(span.text()).toBe("42");
+    btn.simulate("click");
+    expect(span.text()).toBe("43");
+  });
+
+  it("should use latest provided store when multiple stores with the same storeInit is provided", () => {
+    const store1 = createStore(testStore, 1);
+    const store2 = createStore(testStore, 2);
+
+    const Component = () => {
+      const { value, setValue } = useStore(testStore);
+      return (
+        <div>
+          <span>{value}</span>
+          <button onClick={() => setValue(value + 1)}>Increment</button>
+        </div>
+      );
+    }
+
+    const wrapper = mount(
+      <StoreProvider stores={[store1]}>
+        <StoreProvider stores={[store2]}>
+          <Component />
+        </StoreProvider>
+      </StoreProvider>
+    );
+
+    expect(wrapper.find("span").text()).toBe("2");
   });
 });
